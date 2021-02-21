0c8f59f391dae7a7285d9526b829ac5fe84f28ad
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-02-20 20:51:09 -0800

    creates index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Create your views here.
+class Bird:
+    def __init__(self, name, species, description, age):
+        self.name = name
+        self.species = species
+        self.description = description
+        self.age = age
+
+birds = [
+    Bird('Charles', 'Falcon', 'fast flyer', 2),
+    Bird('Rick', 'Pigeon', 'loves letters', 1),
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'birds/index.html', {'birds': birds})
