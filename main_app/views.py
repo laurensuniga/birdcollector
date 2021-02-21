fde6d1d7c9cd2267a009c6f226fc137333f2f511
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-02-20 19:40:13 -0800

    creates about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    pass
+    return HttpResponse('<h1>Hello World</h1>')
+
+def about(request):
+    return render(request, 'about.html')
