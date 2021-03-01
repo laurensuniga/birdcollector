54d008628ffd45cfed3fa2b676ba0b926ee69e07
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-03-01 13:14:12 -0800

    changes details page

--- main_app/views.py
+++ main_app/views.py
@@ -30,30 +30,22 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
   bird = Bird.objects.get(id=bird_id)
-  # exclude from our query all toys associated w/ the current cat
   toys_bird_doesnt_have = Toy.objects.exclude(id__in = bird.toys.all().values_list('id'))
-  # instantiate FeedingForm to be rendered in the template
   feeding_form = FeedingForm()
   return render(request, 'birds/detail.html', {
-    # pass the cat and feeding_form as context
     'bird': bird, 'feeding_form': feeding_form,
     'available_toys': toys_bird_doesnt_have
   })
 
 def add_feeding(request, bird_id):
-	# create the ModelForm using the data in request.POST
   form = FeedingForm(request.POST)
-  # validate the form
   if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
     new_feeding = form.save(commit=False)
     new_feeding.bird_id = bird_id
     new_feeding.save()
   return redirect('detail', bird_id=bird_id)
 
 def assoc_toy(request, bird_id, toy_id):
-  # locate the cat & add the toy by its ID
   Bird.objects.get(id=bird_id).toys.add(toy_id)
   return redirect('detail', bird_id=bird_id)
 
